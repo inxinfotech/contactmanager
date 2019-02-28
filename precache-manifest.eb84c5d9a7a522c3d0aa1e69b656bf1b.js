453aa040a5e74ed1fc92b288646befa7396417ea
unknown <jdks490@gmail.com>
2019-02-28 10:09:27 +0530

    Updates

---
+++ precache-manifest.eb84c5d9a7a522c3d0aa1e69b656bf1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4bf40f27ed8dbd6f9da",
+    "url": "/contactmanager/static/js/runtime~main.e4bf40f2.js"
+  },
+  {
+    "revision": "c78ccdba2179c74d1baa",
+    "url": "/contactmanager/static/js/main.c78ccdba.chunk.js"
+  },
+  {
+    "revision": "59637d1b473ad6ea4515",
+    "url": "/contactmanager/static/js/2.59637d1b.chunk.js"
+  },
+  {
+    "revision": "59637d1b473ad6ea4515",
+    "url": "/contactmanager/static/css/2.7104a32e.chunk.css"
+  },
+  {
+    "revision": "4aafb0d0d1a11d170410f7bfba3b80b3",
+    "url": "/contactmanager/index.html"
+  }
+];
